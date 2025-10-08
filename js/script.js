72c08f827455e055bc7a7c067a884c4174b68311
hermanaaaaaaaa <kcarrillo@centro.edu.mx>
2025-10-08 11:19:01 -0600

    update

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var nombre = "itzel";
+let edad = 21;
+const PI = Math.PI;
+const TAU = Math.PI * 2;
+
+
+
+let esMayorEdad = edad >= 18;
+
+nombre= "juan";
+edad = 21;
+console.log("Hola " + nombre);
+
+function saludar() {
+
+}
