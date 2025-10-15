77c984fa3e4de5e448ad1bef9e62b942d6d69bea
hermanaaaaaaaa <kcarrillo@centro.edu.mx>
2025-10-15 10:40:38 -0600

    update

---
+++ dark-mode.js
@@ -0,0 +1,26 @@
+const toggleThemeBtn = document.getElementById('toggle-theme-btn');
+const colorPicker = document.getElementById('color-picker');
+const backgroundPicker = document.getElementById('background-picker');
+
+
+toggleThemeBtn.addEventListener('click',function (){
+    document.body.classList.toggle('dark-mode'); 
+    if (document.body.classList.contains('dark-mode')){
+
+    toggleThemeBtn.innerText = 'Activar Light Mode';  
+
+} else {
+    toggleThemeBtn.innerText = 'Activar Dark Mode';
+  
+
+});
+
+
+
+colorPicker.addEventListener('change', function() {
+    document.body.style.color = colorPicker.value;
+});
+
+backgroundPicker.addEventListener('inout', function() {
+    document.body.style.backgroundColor = backgroundPicker.value;
+});
